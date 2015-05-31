22d40f17e8f64d73d0287ecb88cdba81e58f1111
proshunsuke <shunsuke0901@gmail.com>
2015-06-01 00:03:03 +0900

    4¤ޤÇ

---
+++ 4/program.js
@@ -0,0 +1,12 @@
+/**
+ * Created by pro on 15/05/31.
+ */
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+import {PI, sqrt, square} from './Math';
+
+console.log(PI);
+console.log(sqrt(+arg1));
+console.log(square(+arg2));
